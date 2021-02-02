fb982a91c2c266a7ffb8a49c2b88b070692a95f8
ayushmankumar7 <ayushmankumar7@gmail.com>
2021-02-02 13:08:37 +0530

    Basic Transformation

---
+++ preprocessing.py
@@ -0,0 +1,29 @@
+import tensorflow as tf 
+
+@tf.function
+def normalize(input_image, input_mask):
+    input_image = tf.cast(input_mask, tf.float32) / 255.0
+    return input_image, input_mask 
+
+@tf.function
+def load_image_train(datapoint, IMG_SIZE = 128):
+    input_image = tf.image.resize(datapoint['image'], (IMG_SIZE, IMG_SIZE))
+    input_mask = tf.image.resize(datapoint['mask'], (IMG_SIZE, IMG_SIZE))
+
+    if tf.random.uniform(())  > 0.5:
+        input_image = tf.image.flip_left_right(input_image)
+        input_mask = tf.image.flip_left_right(input_mask)
+
+    input_image, input_mask = normalize(input_image, input_mask)
+    return input_image, input_mask 
+
+@tf.function
+def load_image_test(datapoint, IMG_SIZE = 128):
+    input_image = tf.image.resize(datapoint['image'], (IMG_SIZE, IMG_SIZE))
+    input_mask = tf.image.resize(datapoint['mask'], (IMG_SIZE, IMG_SIZE))
+    input_image, input_mask = normalize(input_image, input_mask)
+    return input_image, input_mask 
+
+
+
+    
