df2bb1c571da5bc48251e52088a1bafb7e9dcee0
ayushmankumar7 <ayushmankumar7@gmail.com>
2021-02-02 12:39:18 +0530

    Visualize utils

--- utils/visualize.py
+++ utils/visualize.py
@@ -8,7 +8,7 @@ def display_sample(img_list):
     for i in range(len(img_list)):
         plt.subplot(1, len(img_list), i+1)
         plt.title(title[i])
-        plt.imshow(tf.keras.preprocessing.image.array_to_image(img_list[i]))
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(img_list[i]))
         plt.axis('off')
 
     plt.show()
