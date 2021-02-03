ea5b3a2c5794f2bb84e7b416f99e1e552ab9ad18
ayushmankumar7 <ayushmankumar7@gmail.com>
2021-02-03 21:58:01 +0530

    revert back:

--- preprocessing.py
+++ preprocessing.py
@@ -2,8 +2,6 @@ import tensorflow as tf
 
 @tf.function
 def normalize(input_image, input_mask):
-    input_image = tf.image.resize(input_image, (300,300,))
-    input_mask = tf.image.resize(input_mask, (300,300,))
     input_image = tf.cast(input_image, tf.float32) / 255.0
     return input_image, input_mask 
 
