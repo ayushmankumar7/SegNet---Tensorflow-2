57b4a3f566ff274636fe91e0602e317e2ce35e01
ayushmankumar7 <ayushmankumar7@gmail.com>
2021-02-19 09:52:09 +0530

    bug fixed

--- preprocessing.py
+++ preprocessing.py
@@ -2,13 +2,13 @@ import tensorflow as tf
 
 @tf.function
 def normalize(input_image, input_mask):
-    input_image = tf.cast(input_mask, tf.float32) / 255.0
+    input_image = tf.cast(input_image, tf.float32) / 255.0
     return input_image, input_mask 
 
 @tf.function
 def load_image_train(datapoint, IMG_SIZE = 128):
     input_image = tf.image.resize(datapoint['image'], (IMG_SIZE, IMG_SIZE))
-    input_mask = tf.image.resize(datapoint['mask'], (IMG_SIZE, IMG_SIZE))
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], (IMG_SIZE, IMG_SIZE))
 
     if tf.random.uniform(())  > 0.5:
         input_image = tf.image.flip_left_right(input_image)
@@ -20,7 +20,7 @@ def load_image_train(datapoint, IMG_SIZE = 128):
 @tf.function
 def load_image_test(datapoint, IMG_SIZE = 128):
     input_image = tf.image.resize(datapoint['image'], (IMG_SIZE, IMG_SIZE))
-    input_mask = tf.image.resize(datapoint['mask'], (IMG_SIZE, IMG_SIZE))
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], (IMG_SIZE, IMG_SIZE))
     input_image, input_mask = normalize(input_image, input_mask)
     return input_image, input_mask 
 
